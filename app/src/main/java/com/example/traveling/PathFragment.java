bbd7ef72ef57b15ae2ac9e4fecb93dc2ee57e5d0
Titouan-GL <79847323+Titouan-GL@users.noreply.github.com>
2026-04-16 22:04:45 +0200

    final commit of the day

--- app/src/main/java/com/example/traveling/PathFragment.java
+++ app/src/main/java/com/example/traveling/PathFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class PathFragment extends Fragment {
     public PathFragment() {
@@ -16,6 +17,7 @@ public class PathFragment extends Fragment {
 
 
     Button btn_family, btn_disabled, btn_dynamic, btn_heat, btn_cold, btn_rain;
+    LinearLayout btn_food, btn_culture, btn_discovery, btn_activities;
     View view;
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,9 @@ public class PathFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_path, container, false);
         init();
+        setListeners();
+        btn_family.setSelected(true);
+        btn_food.setSelected(true);
         return view;
     }
     private void init() {
@@ -35,6 +40,34 @@ public class PathFragment extends Fragment {
         btn_heat            = view.findViewById(R.id.btn_heat);
         btn_cold            = view.findViewById(R.id.btn_cold);
         btn_rain            = view.findViewById(R.id.btn_rain);
-        btn_family.setSelected(true);
+        btn_food            = view.findViewById(R.id.Restaurants);
+        btn_culture         = view.findViewById(R.id.Culture);
+        btn_discovery       = view.findViewById(R.id.Discovery);
+        btn_activities      = view.findViewById(R.id.Activities);
+    }
+
+    private void setListeners() {
+        btn_disabled.setOnClickListener(v   -> fn_group_modified(btn_disabled));
+        btn_family.setOnClickListener(v     -> fn_group_modified(btn_family));
+        btn_dynamic.setOnClickListener(v    -> fn_group_modified(btn_dynamic));
+        btn_heat.setOnClickListener(v       -> fn_inverse(btn_heat));
+        btn_cold.setOnClickListener(v       -> fn_inverse(btn_cold));
+        btn_rain.setOnClickListener(v       -> fn_inverse(btn_rain));
+        btn_food.setOnClickListener(v       -> fn_inverse(btn_food));
+        btn_culture.setOnClickListener(v    -> fn_inverse(btn_culture));
+        btn_discovery.setOnClickListener(v  -> fn_inverse(btn_discovery));
+        btn_activities.setOnClickListener(v -> fn_inverse(btn_activities));
+    }
+
+    private void fn_inverse(View v){
+        v.setSelected(!v.isSelected());
+    }
+
+    private void fn_group_modified(View v) {
+        btn_dynamic.setSelected(false);
+        btn_family.setSelected(false);
+        btn_disabled.setSelected(false);
+
+        v.setSelected(true);
     }
 }
